b8ab2978dc41453e38d229aeab8b9f0a05994f30
Bryan Schauerte <bryan.schauerte@gmail.com>
2018-03-04 19:14:26 -0700

    Bryan is the coooliest

---
+++ src/layout/SpanningRow/spanningRow.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './spanningRow.css';
+import { Row, Col } from 'antd';
+
+
+class SpanningRow extends Component {
+
+    createContainers(){
+        let sum = 0;
+        if(this.props.sizingArray){
+            let sizingArray = this.props.sizingArray;
+            sizingArray.map( num => {
+                sum += num;
+            });
+            return sizingArray.map( (num, index) => {
+                return (<Col style={{display:'flex', 'justifyContent':'center'}}key={index+'element'} span={
+                    Math.ceil((num / sum) * (this.props.maxwidth || 24))
+                }>
+                    {this.props.children[index]}
+                </Col>)
+            })
+        } else {
+            return null;
+        }
+
+    }
+  render() {
+
+    return (
+        <div className='row-container'>
+            <Row className='row'>
+              {this.createContainers()}
+            </Row>
+      </div>
+    );
+  }
+}
+
+
+export default SpanningRow;

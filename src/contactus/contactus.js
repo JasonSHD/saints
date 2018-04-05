720feaa698e46026805c38466ebb187822d27e02
Camille Bennett <piqusi@gmail.com>
2018-04-05 14:14:46 -0600

    creating contact pages and attaching links

--- src/contactus/contactus.js
+++ src/contactus/contactus.js
@@ -6,34 +6,15 @@ import '../main.css';
 
 class ContactUs extends Component {
 	render() {
-        let stuffs = [
-      {
-        title: "KUFS 2018",
-        content: "Blah Blah info.",
-        imageUrl: "https://s3-us-west-2.amazonaws.com/unknown-saints/KUFS-Flyer1-1.jpg",
-        ratio: [1, 2]
-      },
-      {
-        title: "KUFS 2018 T-Shirt",
-        content: "Our 2018 T-Shirt.",
-        imageUrl: "https://s3-us-west-2.amazonaws.com/unknown-saints/2018-tshirt-design-1a-1.jpg",
-        ratio: [2, 1]
-      }
-    ];
 		return (
-        <div>
-          <Button href="/eventdetails">Upcoming Events</Button>
-          <Button href="/eventdetails">Past Events</Button>
-  		    <SpanningRow sizingArray={stuffs[0].ratio}>
-            <MainBlogImage imgCSS="generalImage" imageUrl={stuffs[0].imageUrl} />
-            <p className="textStyle" >{stuffs[0].content}</p>
-        </SpanningRow>
-        <SpanningRow sizingArray={stuffs[1].ratio}>
-            <p className="textStyle" >{stuffs[1].content}</p>
-            <MainBlogImage imgCSS="verticalFitImage" imageUrl={stuffs[1].imageUrl} />
-        </SpanningRow>
-        </div>
-  		)
+      <div>
+        <ul className="contactUs">
+          <li><a href="https://www.facebook.com/groups/1672438879652518/">Facebook</a></li>
+          <li>Email</li>
+          <li>Mail</li>
+        </ul>
+      </div>
+  	)
 	}
 }
 
